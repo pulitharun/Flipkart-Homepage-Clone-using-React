32e2f9c457fa0c3306380d2a8898fa294feecf00
Puli Tharun <pulitharun21@gmail.com>
2024-04-10 15:14:01 +0530

    Added account button and popover

--- flipkart/src/common/account/index.js
+++ flipkart/src/common/account/index.js
@@ -1,24 +1,37 @@
 import React from "react";
+import { useState } from "react";
+import { Popover, Typography } from "@mui/material";
 
 
 const AccountMenu = () => {
+  const [el,setEl] = useState(null)
+  
+  const handlePopoverOpen = (event) =>{
+    setEl(event.currentTarget)
+
+  }
+  const handlePopoverClose = ()=>{
+    setEl(null)
+  }
+
+
     return (
         <div>
         <Typography
-          aria-owns={open ? 'account-menu-popover' : undefined}
+          aria-owns={el ? 'account-menu-popover' : undefined}
           aria-haspopup="true"
           onMouseEnter={handlePopoverOpen}
           onMouseLeave={handlePopoverClose}
         >
-          Hover with a Popover.
+          Account  <span style= {{lineHeight: 3 , rotate : el ? "0deg" : "180deg",display :"inline-block"}}> ^ </span>
         </Typography>
         <Popover
           id="account-menu-popover"
           sx={{
             pointerEvents: 'none',
           }}
-          open={open}
-          anchorEl={anchorEl}
+          open={!!el}
+          anchorEl={el}
           anchorOrigin={{
             vertical: 'bottom',
             horizontal: 'left',
@@ -30,8 +43,13 @@ const AccountMenu = () => {
           onClose={handlePopoverClose}
           disableRestoreFocus
         >
-          <Typography sx={{ p: 1 }}>I use Popover.</Typography>
+          <Typography sx={{ p: 1 }}>My Profile</Typography>
+          <Typography sx={{ p: 1 }}>Orders</Typography>
+          <Typography sx={{ p: 1 }}>Wishlist</Typography>
+          <Typography sx={{ p: 1 }}>Notifications</Typography>
+          <Typography sx={{ p: 1 }}>Logout</Typography>
         </Popover>
       </div>
     )
 }
+export default AccountMenu
--- flipkart/src/components/header/index.js
+++ flipkart/src/components/header/index.js
@@ -4,6 +4,7 @@ import Logo from "../../assets/logo.png";
 import "./header.css";
 import Searchbar from "../../common/search";
 import Popover from '@mui/material/Popover';
+import AccountMenu from "../../common/account";
 
 const Header = () => {
   return (
@@ -20,6 +21,7 @@ const Header = () => {
 
         <img src={Logo} alt="logo" className="header_logo" />
         <Searchbar />
+        <AccountMenu/>
       </Card>
     </div>
   );
